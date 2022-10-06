8769eba08ea6edadcabfd7481023622484328fd9
saheedsalawu <106780350+saheedsalawu@users.noreply.github.com>
2022-10-06 22:14:36 +0100

    Update free_stack.c

--- free_stack.c
+++ free_stack.c
@@ -1,17 +1,34 @@
 #include "monty.h"
+
 /**
-* free_stack - frees a doubly linked list
-* @head: head of the stack
+* free_stack - function that frees a doubly linked list
+* @head: head pointer to the stack
+*
+* Return: nothing
 */
 void free_stack(stack_t *head)
 {
-	stack_t *aux;
+	stack_t *temp;
 
-	aux = head;
+	temp = head;
 	while (head)
 	{
-		aux = head->next;
+		temp = head->next;
 		free(head);
-		head = aux;
+		head = temp;
 	}
+}
+
+/**
+* f_stack - function that prints the top
+* @head: head of stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
 }
